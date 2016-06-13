56f8bab581bea881ec2af9602f56adb8ecd64c1b
Kim T <kim@bynd.com>
2016-06-13 16:27:12 -0400

    example using posts

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,17 @@
 /*globals require, console, global*/
 
 var connect = require('gulp-connect'),
-    gulp = require('gulp');
+    gulp = require('gulp'),
+    postcss = require('gulp-postcss'),
+    rename = require('gulp-rename'),
+    cssnext = require('postcss-cssnext');
 
 global.paths = {
     src: 'src',
     css: '/**/*.css',
     html: '/**/*.html',
-    js: '/**/*.js'
+    js: '/**/*.js',
+    pcss: '/**/*.pcss'
 };
 
 gulp.task('connect', function () {
@@ -24,11 +28,20 @@ gulp.task('connect', function () {
     });
 });
 
+gulp.task('pcss', function () {
+    'use strict';
+    return gulp.src(global.paths.src + global.paths.pcss)
+            .pipe(postcss([cssnext()]))
+            .pipe(rename({extname: '.css'}))
+            .pipe(gulp.dest(global.paths.src));
+});
+
 gulp.task('watch', function () {
     'use strict';
-    gulp.watch([global.paths.src + global.paths.css], function (event) { console.log('css', event.path); gulp.src(event.path).pipe(connect.reload()); });
-    gulp.watch([global.paths.src + global.paths.html], function (event) { gulp.src(event.path).pipe(connect.reload()); });
-    gulp.watch([global.paths.src + global.paths.js], function (event) { gulp.src(event.path).pipe(connect.reload()); });
+    gulp.watch(global.paths.src + global.paths.css, function (event) { console.log('css', event.path); gulp.src(event.path).pipe(connect.reload()); });
+    gulp.watch(global.paths.src + global.paths.html, function (event) { gulp.src(event.path).pipe(connect.reload()); });
+    gulp.watch(global.paths.src + global.paths.js, function (event) { gulp.src(event.path).pipe(connect.reload()); });
+    gulp.watch(global.paths.src + global.paths.pcss, ['pcss']);
 });
 
 gulp.task('default', ['connect', 'watch']);
